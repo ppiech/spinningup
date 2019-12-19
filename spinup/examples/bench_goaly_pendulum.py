7098be17ab17e2617126c941d06f6da0b0d3a74b
Pawel Piech <ppiech@fitbit.com>
2019-12-18 21:40:37 -0800

    Experiment g1

--- spinup/examples/bench_goaly_pendulum.py
+++ spinup/examples/bench_goaly_pendulum.py
@@ -5,20 +5,20 @@ import tensorflow as tf
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cpu', type=int, default=1)
+    parser.add_argument('--cpu', type=int, default=6)
     parser.add_argument('--num_runs', type=int, default=10)
     args = parser.parse_args()
 
     print(args.num_runs)
 
-    eg = ExperimentGrid(name='goaly-bench')
+    eg = ExperimentGrid(name='g1')
     eg.add('env_name', 'Pendulum-v0', '', True)
     eg.add('seed', [10*i for i in range(args.num_runs)])
     eg.add('epochs', 200)
     eg.add('steps_per_epoch', 1200)
-    eg.add('goal_octaves', [3])
-    eg.add('invese_buffer_size', [3])
-    eg.add('goal_discount_rate', [0.01])
+    eg.add('goal_octaves', [2, 5])
+    eg.add('invese_buffer_size', [1, 5])
+    eg.add('goal_discount_rate', [0.01, 0.001])
     eg.add('split_action_and_goal_models', [False], '')
     eg.add('no_step_reward', [False], '')
     eg.add('forward_error_for_stability_reward', [False], '')
