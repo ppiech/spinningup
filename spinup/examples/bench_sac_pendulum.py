a3dda222e6c1bd3ed3006d4b2e9ecd65d7f3888e
Pawel Piech <ppiech@fitbit.com>
2020-01-01 16:24:38 -0800

    Add ppo and sac bench scripts

---
+++ spinup/examples/bench_sac_pendulum.py
@@ -0,0 +1,19 @@
+from spinup.utils.run_utils import ExperimentGrid
+from spinup import sac
+import tensorflow as tf
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--num_runs', type=int, default=10)
+    args = parser.parse_args()
+
+    eg = ExperimentGrid(name='sac')
+    eg.add('env_name', 'Pendulum-v0', '', True)
+    eg.add('seed', [10*i for i in range(args.num_runs)])
+    eg.add('epochs', 1000)
+    eg.add('steps_per_epoch', 4000)
+    eg.add('ac_kwargs:hidden_sizes', [(32,32)], 'hid')
+    eg.add('ac_kwargs:activation', [tf.nn.relu], '')
+    eg.run(sac, num_cpu=args.cpu)
