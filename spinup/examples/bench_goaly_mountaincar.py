6ff99e35964d59764b79717c03511bce1d91f800
Pawel Piech <ppiech@fitbit.com>
2020-01-13 21:59:48 -0800

    Add notebooks

--- spinup/examples/bench_goaly_mountaincar.py
+++ spinup/examples/bench_goaly_mountaincar.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
     parser.add_argument('--num_runs', type=int, default=1)
     args = parser.parse_args()
 
-    eg = ExperimentGrid(name='forward_error_reward')
+    eg = ExperimentGrid(name='bin_goals_by_section_split')
     eg.add('env_name', 'MountainCar-v0', '', True)
     eg.add('seed', [10*i for i in range(args.num_runs)])
     eg.add('epochs', 100)
     eg.add('steps_per_epoch', 2000)
     eg.add('goal_octaves', 5)
-    eg.add('goal_discount_rate', 0.0)
     eg.add('no_step_reward', [True], '')
+
     eg.add('split_action_and_goal_models', [True])
     eg.add('ac_kwargs:hidden_sizes', [(32,32)], 'hid')
     eg.add('ac_kwargs:activation', [tf.nn.relu], '')
